2d15e2ddd8e19baf07a30971e171ba4fa9dc4601
brittenyoko <bokoromachuonye@college.harvard.edu>
2020-05-17 22:29:29 -0400

    loading animation

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const skillsLink = document.querySelector(".page-header li:nth-child(1) a");
-const skillsClose = document.querySelector(".skills-close");
-const windowOpened = "window-opened";
-
-skillsLink.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.toggle(windowOpened);
-});
-
-skillsClose.addEventListener("click", () => {
-  document.body.classList.toggle(windowOpened);
+$(window).on("load",function(){
+  $(".loader-wrapper").fadeOut("slow");
 });
